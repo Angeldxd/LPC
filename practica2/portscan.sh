d92181d607e9a5895fcef3e490ccdded46a7ca9b
Angeldxd <112783798+Angeldxd@users.noreply.github.com>
2022-09-16 22:03:45 -0500

    Add files via upload

---
+++ practica2/portscan.sh
@@ -0,0 +1,12 @@
+host=$1
+firstport=$2
+lastport=$3
+
+function scan {
+for((counter=$firstport; counter<=$lastport; counter++))
+do
+	(echo >/dev/tcp/$host/$counter) > /dev/null 2>&1 && echo "$counter open"
+done
+}
+
+scan
