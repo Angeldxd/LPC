6534283a60f9070c9290afb403b4f4500261dd8d
Angeldxd <112783798+Angeldxd@users.noreply.github.com>
2022-10-31 23:40:24 -0600

    practica6

---
+++ practica6/nmap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+hostname=`hostname -i`
+hostname=`nmap $hostname`
+
+ifconfig=`curl ifconfig.me`
+ifconfig=`nmap $ifconfig`
+
+echo -e "$hostname \n$ifconfig" > datos.txt
+
+base64 datos.txt > Encodedatos.txt
+
